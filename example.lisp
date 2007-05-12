1127591a046af224af0eb01414b28649f1c8b483
David Lichteblau <david@lichteblau.com>
2007-05-12 22:39:46 +0200

    dokumentation

---
+++ example.lisp
@@ -0,0 +1,5 @@
+(atdoc:generate-documentation '(:blocks-world :blocks-world-goals)
+			      "/home/david/src/lisp/atdoc/example/"
+			      :index-title "Blocks World API reference"
+			      :heading "The Blocks World"
+			      :css "orange-sans.css")
