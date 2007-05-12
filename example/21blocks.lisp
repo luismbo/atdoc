682a1e50971964868dd94b8ada5f32be7add29af
David Lichteblau <david@lichteblau.com>
2007-05-13 00:43:54 +0200

    blocks world fertig dokumentiert

--- example/21blocks.lisp
+++ example/21blocks.lisp
@@ -21,12 +21,13 @@
 ;;;;   - removed the #-clos and #+gclisp conditionalizations
 ;;;;   - added defvars for *HAND* and TABLE as well as B1 to L8
 ;;;;   - moved 21blocks.dta into this file
+;;;;   - added documentation
 
 (in-package :blocks-world)
 
 (defvar *hand*)
 
-(defvar table)
+(defvar table nil "@unexport{}")
 (defvar b1)
 (defvar b2)
 (defvar b3)
@@ -39,32 +40,209 @@
 
 ;;;; PROCEDURES
 
+(defgeneric block-name (instance)
+  (:documentation
+   "@arg[instance]{a @class{basic-block}}
+    @return{a symbol}
+    @short{Returns the block's name, a symbol.}
+
+    In the examples from the book, a global variable of this name is used
+    to refer to @code{instance}."))
+
+(defgeneric block-width (instance)
+  (:documentation
+   "@arg[instance]{a @class{basic-block}}
+    @return{an integer}
+    @short{Returns the block's width.}
+
+    The size of a block is specified as width and height, and determines which
+    parts of the world this block occupies.  No other objects can be
+    placed to an overlapping position.
+
+    @see{block-position}
+    @see{block-height}"))
+
+(defgeneric block-height (instance)
+  (:documentation
+   "@arg[instance]{a @class{basic-block}}
+    @return{an integer}
+    @short{Returns the block's height.}
+
+    The size of a block is specified as width and height, and determines which
+    parts of the world this block occupies.  No other objects can be
+    placed to an overlapping position.
+
+    @see{block-position}
+    @see{block-width}"))
+
+(defgeneric block-position (instance)
+  (:documentation
+   "@arg[instance]{a @class{basic-block}}
+    @return{a list of two integers}
+    @short{Returns the block's position.}
+
+    The position of a block is specified as a list of its x and y coordinates,
+    where the first axis runs along the table, and the second axis points
+    upwards towards the hand.
+
+    Together with the block's width and height, the position
+    determines which
+    parts of the world this block occupies.  No other objects can be
+    placed to an overlapping position.
+
+    @see{block-height}
+    @see{block-width}
+    @see{hand-position}"))
+
+(defgeneric block-supported-by (instance)
+  (:documentation
+   "@arg[instance]{a @class{basic-block}}
+    @return{nil, or a block}
+    @short{Returns the block this instance has been placed onto.}
+
+    All blocks except for the table sit on top of another block, which
+    supports them.
+
+    @see{block-support-for}"))
+
 (defclass basic-block ()
   ((name :accessor block-name :initarg :name)
    (width :accessor block-width :initarg :width)
    (height :accessor block-height :initarg :height)
    (position :accessor block-position :initarg :position)
-   (supported-by :accessor block-supported-by :initform nil)))
+   (supported-by :accessor block-supported-by :initform nil))
+  (:documentation
+   "@short{The superclass of all objects in the Blocks World
+    (not including the hand).}
 
-(defclass movable-block (basic-block) ())
+    Subclasses of @code{basic-block} characterize different kinds of objects,
+    and have different properties.
+
+    They all have a name, given as
+    @slot{block-name} and in the examples from the book, a global variable
+    of that name is used to refer to them.
+
+    Since this chapter is an explanation of CLOS, no specific
+    constructor function is defined, and users may call @code{make-instance}
+    directly.
+
+    @see-slot{block-width}
+    @see-slot{block-height}
+    @see-slot{block-position}
+    @see-slot{block-supported-by}"))
+
+(defclass movable-block (basic-block) ()
+  (:documentation
+   "@short{The superclass of objects in the Blocks World that can be moved
+    by the hand.}
+
+    This class is mixed into all blocks except for the @class{table}."))
+
+(defgeneric block-support-for (instance)
+  (:documentation
+   "@arg[instance]{a @class{load-bearing-block}}
+    @return{a list of blocks}
+    @short{Returns the blocks that have been placed onto this instance.}
+
+    @see{block-supported-by}"))
 
 (defclass load-bearing-block (basic-block) 
-  ((support-for :accessor block-support-for :initform nil)))
+  ((support-for :accessor block-support-for :initform nil))
+  (:documentation
+   "@short{The superclass of objects in the Blocks World that other blocks
+    can be placed onto.}
 
-(defclass brick (movable-block load-bearing-block) ())
+    This class is mixed into most blocks, except for the @class{wedge}
+    and the @class{ball}.
 
-(defclass wedge (movable-block) ())
-(defclass ball (movable-block) ())
+    @see-slot{block-support-for}"))
 
-(defclass table (load-bearing-block) ())
+(defclass brick (movable-block load-bearing-block) ()
+  (:documentation
+   "@short{A useful movable building block with a flat top.}
+
+    Because this block has a flat top, it supports other blocks."))
+
+(defclass wedge (movable-block) ()
+  (:documentation
+   "@short{An interesting movable building block.}
+
+    Because this block doesn't have a flat top, it cannot
+    support other blocks."))
+(defclass ball (movable-block) ()
+  (:documentation
+   "@short{The block is a sphere.}
+
+    Because this block doesn't have a flat top, it cannot
+    support other blocks."))
+
+(defclass table (load-bearing-block) ()
+  (:documentation
+   "@short{The table supporting the rest of the world.}
+
+    The entire rest of the world sits on this table.  The table itself
+    cannot be moved.
+
+    For each world, this class is meant to be a singleton."))
+
+(defgeneric hand-name (instance)
+  (:documentation
+   "@arg[instance]{a @class{hand}}
+    @return{a symbol}
+    @short{Returns the hand's name, a symbol.}
+
+    @begin{implementation-node}
+    The hand is always called @code{blocks-world::*hand*}.
+    @end{implementation-node}"))
+
+(defgeneric hand-position (instance)
+  (:documentation
+   "@arg[instance]{a @class{hand}}
+    @return{a list of two integers}
+    @short{Returns the hand's position.}
+
+    The position of a hand is specified as a list of its x and y coordinates,
+    where the first axis runs along the table, and the second axis points
+    upwards towards the hand.
+
+    @see{block-position}"))
+
+(defgeneric hand-grasping (instance)
+  (:documentation
+   "@arg[instance]{a @class{hand}}
+    @return{a @class{movable-block}, or nil}
+    @short{Returns the block the hand is currently holding.} "))
 
 (defclass hand ()
   ((name :accessor hand-name :initarg :name)
    (position :accessor hand-position :initarg :position)
-   (grasping :accessor hand-grasping :initform nil)))
+   (grasping :accessor hand-grasping :initform nil))
+  (:documentation
+   "@short{The hand that moves the world.}
+
+    This hand can be used to move every @class{movable-block}.
+
+    @see-slot{hand-name}
+    @see-slot{hand-position}
+    @see-slot{hand-grasping}"))
 
 (defmethod block-support-for ((object basic-block))
   nil)
+
+(defgeneric put-on (object support)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @arg[support]{a @class{basic-block}}
+    @return{a boolean}
+    @short{Move block @code{object} onto block @code{support}.}
+
+    Prints the steps taken and returns T or prints an error 
+    message and returns nil.
+
+    @see{get-space}
+    @see{grasp}
+    @see{move}
+    @see{ungrasp}"))
 
 (defmethod put-on ((object movable-block) (support basic-block))
   (if (get-space object support)
@@ -75,9 +253,30 @@
               (block-name object)
               (block-name support))))
 
+(defgeneric get-space (object support)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @arg[support]{a @class{basic-block}}
+    @return{undocumented, but non-nil}
+    @short{Find or make space on support for object.}
+
+    @see{find-space}
+    @see{make-space}"))
+
 (defmethod get-space ((object movable-block) (support basic-block))
   (or (find-space object support)
       (make-space object support)))
+
+(defgeneric grasp (object)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @return{t}
+    @short{Grasps the block using the hand.}
+
+    Makes sure to ungrasp the block currently grasped, if any.
+
+    @see{ungrasp}
+    @see{hand}"))
 
 (defmethod grasp ((object movable-block))
   (unless (eq (hand-grasping *hand*) object)
@@ -92,14 +291,45 @@
     (setf (hand-grasping *hand*) object))
   t)
 
+(defgeneric ungrasp (object)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @return{a boolean}
+    @short{Ungrasps the block if hand is holding it.}
+
+    Returns t if successful, or nil if hand didn't hold this block.
+
+    @see{grasp}
+    @see{hand}"))
+
 (defmethod ungrasp ((object movable-block))
   (when (block-supported-by object)
     (format t "~&Ungrasp ~a." (block-name object))
     (setf (hand-grasping *hand*) nil)
     t))
 
+(defgeneric get-rid-of (object)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @return{unspecified}
+    @short{Moves @code{object} onto the @class{table}.}
+
+    @see{put-on}"))
+
 (defmethod get-rid-of ((object movable-block))
   (put-on object table))
+
+(defgeneric make-space (object support)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @arg[support]{a @class{basic-block}}
+    @return{undocumented, but non-nil}
+    @short{Make space on support for object.}
+
+    Takes all necessary actions to make space available.
+
+    @see{get-space}
+    @see{find-space}"))
 
 (defmethod make-space ((object movable-block) (support basic-block))
   (dolist (obstruction (block-support-for support))
@@ -107,9 +337,29 @@
     (let ((space (find-space object support)))
       (when space (return space)))))
 
+(defgeneric clear-top (support)
+  (:documentation
+   "@arg[support]{a @class{load-bearing-block}}
+    @return{nil}
+    @short{Make space on top of this object.}
+
+    Removes all blocks @code{support} is supporting.
+
+    @see{get-rid-of}
+    @see{block-support-for}"))
+
 (defmethod clear-top ((support load-bearing-block))
   (dolist (obstacle (block-support-for support) t)
     (get-rid-of obstacle)))
+
+(defgeneric remove-support (object)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @return{a boolean}
+    @short{Note that @code{object} has been taken from @code{support}.}
+
+    This function maintains the slots @fun{block-supported-by}
+    and @fun{block-support-for}."))
 
 (defmethod remove-support ((object movable-block))
   (let ((support (block-supported-by object)))
@@ -118,6 +368,16 @@
             (remove object (block-support-for support)))
       (setf (block-supported-by object) nil)
       t)))
+
+(defgeneric add-support (object support)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @arg[support]{a @class{basic-block}}
+    @return{a boolean}
+    @short{Note that @code{object} has been put onto @code{support}.}
+
+    This function maintains the slots @fun{block-supported-by}
+    and @fun{block-support-for}."))
 
 (defmethod add-support ((object movable-block)
                         (support basic-block))
@@ -128,6 +388,15 @@
   (push object (block-support-for support))
   (setf (block-supported-by object) support)
   t)
+
+(defgeneric move (object support)
+  (:documentation
+   "@arg[object]{a @class{movable-block}}
+    @arg[support]{a @class{load-bearing-block}}
+    @return{a boolean}
+    @short{Move block @code{object} onto block @code{support}.}
+
+    This is a helper function for @fun{put-on}."))
 
 (defmethod move :before ((object movable-block) ignored-parameter)
   (let ((support (block-supported-by object)))
@@ -163,6 +432,15 @@
   (setf (hand-position *hand*) (top-location object)))
 
 (defun find-space (object support)
+  "@arg[object]{a @class{movable-block}}
+    @arg[support]{a @class{basic-block}}
+    @return{undocumented or nil}
+    @short{Find space on support for object.}
+
+    Returns nil if no space could be found.
+
+    @see{get-space}
+    @see{make-space}"
   (dotimes (offset (+ 1 (- (block-width support)
                            (block-width object))))
     (unless (intersections-p object offset
--- example/package.lisp
+++ example/package.lisp
@@ -21,7 +21,6 @@
 	   #:ball
 	   #:table
 
-	   #:*hand*
 	   #:hand
 	   #:hand-name
 	   #:hand-position
@@ -36,10 +35,76 @@
 	   #:clear-top
 	   #:remove-support
 	   #:add-support
-	   #:move
-	   #:find-space
-	   #:intersections-p
-	   #:top-location))
+	   #:move)
+  (:documentation
+   "This package contains the source code of chapter 21, @em{\"The Blocks
+    World with Classes and Methods\"} from
+    @a[http://www.amazon.com/Lisp-3rd-Patrick-Winston/dp/0201083191]{
+      Lisp (3rd edition)}
+    by Winston and Horn.
+    @begin[A picture of the world]{section}
+    The block objects represent a world that \"looks\" like this:
+    @begin{pre}
+/----\\    ^    /---------\\      ^
+| b4 |   /  \\  |         |     / \\
+\\____/  /_w7_\\ |         |     / \\
+/----\\  /----\\ |         |    /   \\  /--------\\        /^\\
+| b1 |  | b2 | | b3      |    /   \\  | b6     |       (l8 )
+\\____/  \\____/ \\_________/   /_w5__\\ \\________/        \\./
++-----------------------------------------------------------+
+|                                                           |
++-----------------------------------------------------------+
+    @end{pre}
+    @end{section}
+    @begin[Example]{section}
+    In the initial configuration, where all blocks have been placed directly
+    on the table (not shown), @fun{put-on} will move the objects like this:
+    @begin{pre}
+BLOCKS-WORLD> (put-on b1 b2)
+Move hand to pick up B1 at location (1 2).
+Grasp B1.
+Removing support relations between B1 and TABLE.
+Move B1 to top of B2 at location (2 2).
+Adding support relations between B1 and B2.
+Ungrasp B1.
+T
+    @end{pre}
+    @end{section}
+    @begin[The different kinds of blocks]{section}
+    Movable blocks than can be moved onto load supporting blocks.  Using
+    multiple inheritance, there are also blocks that can do both.
+
+    @aboutclass{basic-block}
+    @aboutclass{load-bearing-block}
+    @aboutclass{movable-block}
+    @end{section}
+    @begin[Block properties]{section}
+    Slot readers:
+
+    @aboutfun{block-name}
+    @aboutfun{block-position}
+    @aboutfun{block-width}
+    @aboutfun{block-height}
+    @aboutfun{block-supported-by}
+    @aboutfun{block-support-for}
+    @end{section}
+    @begin[Concrete block classes]{section}
+    These are the blocks found in our world:
+
+    @aboutclass{table}
+    @aboutclass{brick}
+    @aboutclass{wedge}
+    @aboutclass{ball}
+    @end{section}
+    @begin[The hand]{section}
+    The hand is movable.  It can hold at most one block.
+
+    @aboutclass{hand}
+    @aboutfun{hand-name}
+    @aboutfun{hand-position}
+    @aboutfun{hand-grasping}
+    @end{section}
+"))
 
 (defpackage :blocks-world-goals
   (:use :cl :blocks-world)
@@ -53,4 +118,17 @@
 	   #:define-history-method
 	   #:show-simple-tree
 	   #:find-action
-	   #:tell-why))
+	   #:tell-why)
+  (:documentation
+   "This package contains the source code of chapter 22, @em{\"Answering
+    Questions about Goals\"} from
+    @a[http://www.amazon.com/Lisp-3rd-Patrick-Winston/dp/0201083191]{
+      Lisp (3rd edition)}
+    by Winston and Horn.
+
+    @begin[Lots of undocumented functions]{section}
+    I was too lazy to document this package, which is why all its functions
+    have a big fat \"undocumented\" warning.
+
+    This package's page also looks rather empty and sad.
+    @end{section}"))
